55c7844ded250aa7f22ef56d8db251a9e711c2df
ruitokatsuki <ruit.riot.114@gmail.com>
2020-07-25 11:49:49 +0900

    finish2-8

---
+++ start2-8/for解説.rb
@@ -0,0 +1,36 @@
+puts "計算をはじめます"
+puts "何回繰り返しますか？"
+times = gets.to_i
+
+for i in 1..times do
+  puts "#{i}回目の計算"
+  # ここに四則計算の処理を書く
+end
+
+
+puts "1回目の計算"
+puts "2回目の計算"
+puts "3回目の計算"
+
+---
+
+for i in 1..5 do
+  puts "#{i}回目の計算"
+end
+
+---
+
+i = 1
+puts "#{i}回目の計算"
+
+i = 2
+puts "#{i}回目の計算"
+
+i = 3
+puts "#{i}回目の計算"
+
+i = 4
+puts "#{i}回目の計算"
+
+i = 5
+puts "#{i}回目の計算"
